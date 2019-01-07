7c151830781f8f3a5b1653c8d155b8c9bd612087
vvcx <vvcx598@gmail.com>
2019-01-07 21:29:03 +0800

    scrollRecommend

---
+++ js/recommendTest.js
@@ -0,0 +1,26 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("fade");   //slides {0,1,2,3,4}共5个参数
+  var dots = document.getElementsByClassName("gMark");          //dots {0,1,2,3,4}共5个参数
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
